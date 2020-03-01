4734372e083b22d0991389117d5b42d534a9dfde
Peter Christensen <tesercat@pakaa.net>
2020-03-01 13:15:53 -0600

    Add nginx role.

---
+++ roles/nginx/library/certbot.py
@@ -0,0 +1,63 @@
+""" Ansible module to run the certbot script. """
+import os
+import subprocess
+from subprocess import PIPE
+from ansible.module_utils.basic import AnsibleModule
+
+
+def _certbot(acme_dir, admin_email, hostname, other_hostnames):
+    """ Run the certbot command (webroot plugin) to obtain a cert.
+
+    Certbot is a Python library, but it seems to have been designed to be
+    used only via cli, not as an API. Use subprocess.
+    """
+
+    # Check that the cert for the host doesn't already exist.
+    path = os.path.join('/etc/letsencrypt/live', hostname)
+    if os.path.exists(path):
+        return False
+
+    # Expect one hostname and a space-delimited set of other hostnames.
+    hosts = [hostname] + other_hostnames.split()
+
+    # Run the certbot command.
+    cmd = 'certbot certonly --agree-tos --webroot -n -m %s -w %s %s'
+    hostparms = '-d %s' % ' -d '.join(hosts)
+    cmd = cmd % (admin_email, acme_dir, hostparms)
+    popen = subprocess.Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
+    _, err = popen.communicate()
+
+    # Check that the cert exists.
+    if os.path.exists(path):
+        return True
+    raise AssertionError(err)
+
+
+def main():
+    """ Init and run module. """
+    module = AnsibleModule(argument_spec={
+        'acme_dir': {'required': True},
+        'admin_email': {'required': True},
+        'hostname': {'required': True},
+        'other_hostnames': {'required': True},  # Space delimited.
+    })
+    exit_json = {
+        'changed': False,
+        'failed': False,
+        'failure': None
+    }
+    try:
+        exit_json['changed'] = _certbot(
+            module.params['acme_dir'],
+            module.params['admin_email'],
+            module.params['hostname'],
+            module.params['other_hostnames'],
+        )
+    except (OSError, AssertionError) as err:
+        exit_json['failed'] = True
+        exit_json['failure'] = repr(err)
+    module.exit_json(**exit_json)
+
+
+if __name__ == '__main__':
+    main()
